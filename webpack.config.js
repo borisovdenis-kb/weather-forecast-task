cf79ed42d662d9762f97ef83328a7a2f3810fc59
Denis Borisov <borisovdenis-kb@yandex.ru>
2019-01-06 18:00:14 +0400

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/script-2.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  watch: true,
+  devtool: 'source-map'
+};
